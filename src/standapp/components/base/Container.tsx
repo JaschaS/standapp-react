5bfcd3076852d0a4681091aeb637f9bef67ee008
Jascha Scholz <jascha_scholz@web.de>
2021-11-09 00:46:12 +0100

    container has now an attribute style

--- src/standapp/components/base/Container.tsx
+++ src/standapp/components/base/Container.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 export interface ContainerProps {
     children?: React.ReactNode;
+    style?: React.CSSProperties;
     className?: string;
 }
 
 export function Container(props: ContainerProps) {
-    return <div className={props.className}>
+    return <div className={props.className} style={props.style}>
         {props.children}
     </div>;
 }
