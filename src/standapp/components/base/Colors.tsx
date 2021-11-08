1c541ee8c0f37658ff07acd2f83cb2a86a2024b2
Jascha Scholz <jascha_scholz@web.de>
2021-11-09 00:46:39 +0100

    adds app colors as const variables to the project

---
+++ src/standapp/components/base/Colors.tsx
@@ -0,0 +1,7 @@
+export const StandardBlue = "#263238";
+export const JaschaRed = "#FF685F";
+export const BabyBlue = "#E0F3F8";
+export const FiftyShades = "#E8E8E8";
+export const WeißerAlsWeiß = "#FFFFFF";
+export const AudiGray = "#666666";
+export const DarkGray = "#979797";
