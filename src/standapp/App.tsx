d0b9c721d7938553b9b431c7043e51c42069064a
Jascha Scholz <jascha_scholz@web.de>
2021-11-08 23:57:31 +0100

    add app headings and copy to the project

--- src/standapp/App.tsx
+++ src/standapp/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.module.css';
+import { H1, H2, H3, H4, H5 } from './components/base/Header';
+import { Copy1, Copy2, Copy3 } from './components/base/Copy';
 
 export default function App() {
   return (
     <div>
+      <H1>Hey, this is Jascha</H1>
+      <H2>Hey, this is Jascha</H2>
+      <H3>Hey, this is Jascha</H3>
+      <H4>Hey, this is Jascha</H4>
+      <H5>Hey, this is Jascha</H5>
+
+
+      <Copy1>Hey, this is Jascha</Copy1>
+      <Copy2>Hey, this is Jascha</Copy2>
+      <Copy3>Hey, this is Jascha</Copy3>
     </div>
   );
 }
---
+++ src/standapp/components/base/Copy.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface CopyProps {
+    children?: React.ReactNode;
+    className?: string;
+}
+
+export function Copy1(props: CopyProps) {
+    return <p className={props.className} style={style(16)}>
+        {props.children}
+    </p>;
+}
+
+export function Copy2(props: CopyProps) {
+    return <p className={props.className} style={style(12)}>
+        {props.children}
+    </p>;
+}
+
+export function Copy3(props: CopyProps) {
+    return <p className={props.className} style={style(10)}>
+        {props.children}
+    </p>;
+}
+
+function style(fontSize: number) {
+    return {
+        fontFamily: 'Inter',
+        fontSize: `${fontSize}px`,
+        fontWeight: 400,
+        letterSpacing: '0'
+    };
+}
