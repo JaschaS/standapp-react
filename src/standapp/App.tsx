f1e4927d4120d07a229303fa478547ff1a0024c8
Jascha Scholz <jascha.scholz@audi.de>
2021-11-09 11:03:47 +0100

    using sass instead of css for all styles

--- src/standapp/App.tsx
+++ src/standapp/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.module.css';
+import './App.module.scss';
 import { H1, H2, H3, H4, H5 } from './components/base/Header';
 import { Copy1, Copy2, Copy3 } from './components/base/Copy';
 import { Container } from './components/base/Container';
